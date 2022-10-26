347ecda0f7758255c95262f7bdfae3db48a1cd64
devon gibson <devongibs2@gmail.com>
2022-10-26 01:06:07 -0700

    updates

---
+++ menu.js
@@ -0,0 +1,9 @@
+class Employee{
+    constructor(firstName, lastName , position, age , company){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.postion = position;
+        this.age = age;
+        this.company = company;
+    }
+}
