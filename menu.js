f377ec864cc710d9a6d7d6ac86187fafb36b9a34
devon gibson <devongibs2@gmail.com>
2022-10-28 00:17:49 -0700

    updates

--- menu.js
+++ menu.js
@@ -1,9 +1,27 @@
-class Employee{
-    constructor(firstName, lastName , position, age , company){
+class Employee {
+    constructor(firstName, lastName, position, age, company) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.postion = position;
         this.age = age;
-        this.company = company;
+    }
+    describe() {
+        return `${this.firstName} is a ${this.position} `;
     }
 }
+
+class Company {
+    constructor(name) {
+        this.name = name;
+        this.restaurant = [];
+    }
+
+    addEmployee(employee) {
+        if (employee instanceof Employee) {
+            this.restaurant.push(employee);
+        }else {
+            throw new Error(`You can only add an instance of Employee. Argument is not an employee:${player}`);
+        }
+    }
+}
+
